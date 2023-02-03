f55f82bae13be10c9eb3e8b53031e22014bdadde
DriverStation <tekdemo+driverstation@gmail.com>
2023-02-02 18:41:10 -0800

    Initial Tower setup

--- src/main/java/frc/robot/subsystems/Tower.java
+++ src/main/java/frc/robot/subsystems/Tower.java
@@ -4,15 +4,60 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxPIDController.ArbFFUnits;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Tower extends SubsystemBase {
+
+  CANSparkMax motor = new CANSparkMax(19, MotorType.kBrushless);
+  RelativeEncoder encoder = motor.getEncoder();
+  SparkMaxPIDController pid = motor.getPIDController();
+  double ks = 0; //Volts
+
   /** Creates a new Tower. */
-  public Tower() {}
+  public Tower() {
+    encoder.setPositionConversionFactor(10);
+    encoder.setPosition(0); //TODO read current position from absdolute encoder
+
+    motor.setSoftLimit(SoftLimitDirection.kForward, 20);
+    motor.enableSoftLimit(SoftLimitDirection.kForward, true);
+    //TODO set limit for kreverse
+
+    pid.setP(0);
+    pid.setI(0);
+    pid.setD(0);
+    pid.setFF(0);
+
+    setAngle(0);
+  }
+
+  public void setAngle(double angle){
+    pid.setReference(0, ControlType.kPosition, 0, ks, ArbFFUnits.kVoltage);
+  }
+
+  public void getAngle(){
+    encoder.getPosition();
+  }
+  
+  public boolean isOnTarget(){
+    return isOnTarget(3);
+  }
+  public boolean isOnTarget(double degrees){
+    return true;
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    //print useful development values
   }
 
 }
