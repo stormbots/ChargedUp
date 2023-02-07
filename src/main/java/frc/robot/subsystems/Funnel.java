7de79b1f36c4b9bea42ba699df59778e1313d742
Frankreed <baoanhtdn@gmail.com>
2023-02-06 20:46:04 -0800

    Basic Funnel Code Created

--- src/main/java/frc/robot/subsystems/Funnel.java
+++ src/main/java/frc/robot/subsystems/Funnel.java
@@ -4,16 +4,43 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Funnel extends SubsystemBase {
-  /** Creates a new Flipper. */
-  public Funnel(){
-
+  public CANSparkMax leftFunnelMotor = new CANSparkMax(10, MotorType.kBrushless);
+  public CANSparkMax rightFunnelMotor = new CANSparkMax(11, MotorType.kBrushless);
+  public Solenoid rightSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 6);
+  public Solenoid leftSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 5);
+  
+  /** Creates a new Funnel. */
+  public Funnel() {
+    leftFunnelMotor.follow(rightFunnelMotor, true);
+    rightSolenoid.set(true);
+    leftSolenoid.set(true);
+  
   }
-
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  public void down(){
+    rightSolenoid.set(false);
+    leftSolenoid.set(false);
+  }
+  public void up(){
+    rightSolenoid.set(true);
+    leftSolenoid.set(true);
+  }
+  public void rotateConesForward(){
+    rightFunnelMotor.set(0.2);
+  }
+  public void rotateConesBackward(){
+    rightFunnelMotor.set(0.2);
+  }
+  
 }
